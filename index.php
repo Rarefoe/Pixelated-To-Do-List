3c29bea494861e4fb072647169f4610185cd3b1d
Aarzuna <aarjustarlook@hotmail.com>
2026-01-04 15:32:21 +0545

    video bg

--- index.php
+++ index.php
@@ -6,7 +6,18 @@
     <title>Pixelated To-Do-List</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<body>
+<body class="bg">
+ 
+<div class="video-background">
+  <video autoplay muted loop playsinline poster="PinDown.io_@lydh89_1767518163.gif">
+    <source src="" type="video/mp4">PinDown.io_@lydh89_1767518163.gif
+    <!-- You can add alternative formats for better browser compatibility -->
+    <source src="PinDown.io_@lydh89_1767518163.gif" type="video/webm">
+    Your browser does not support the video tag.
+  </video>
+</div>
+ 
+
     <header>
 
     <h2 class="pixe"> <b>PIXE-LATED</b></h2>
