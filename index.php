a01d7fff56b10f47d3518e5c539ab33891bee0c4
Aarzuna <aarjustarlook@hotmail.com>
2026-01-05 12:24:29 +0545

    major css changes

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,32 +7,38 @@
     <title>QuestLog.io</title>
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
 </head>
-<body class="bg">
- 
+<body>
+
+   
 <div class="video-background">
-  <video autoplay muted loop playsinline poster="PinDown.io_@lydh89_1767518163.gif">
+
+  <video autoplay  loop playsinline poster="PinDown.io_@lydh89_1767518163.gif">
+
     <source src="" type="video/mp4">PinDown.io_@lydh89_1767518163.gif
 
+
+
     <source src="PinDown.io_@lydh89_1767518163.gif" type="video/webm">
-    
+
+   
+
   </video>
+
 </div>
- 
 
-    <header>
-
-    <h2 class="pixe"> <b>QUEST-LOG</b></h2>
-   <div id="nav"> <nav class="navigation">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
-
-<button class="btn-login-popup">Login</button>
-    </nav>
-    </div>
+    <header class="main-header">
+        <div class="logo">
+            <h2 class="pixe">QUEST-LOG...</h2>
+        </div>
+        <nav class="navigator">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+            <button class="btn-login-popup">Login</button>
+        </nav>
     </header>
 
 </body>
