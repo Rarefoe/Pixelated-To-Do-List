9620051f43ca3e892d03a76c73060b4e722e898f
Aarzuna <aarzuna52@gmail.com>
2026-01-08 13:24:03 +0545

    minor html fixes

--- index.php
+++ index.php
@@ -41,45 +41,40 @@
             <button class="btn-login-popup">Login</button>
         </nav>
     </header>
-    <div class="wrapper">
-        <div class="form-box">
-            <div class="content">
-                <h2 class="log-box">Player Login...</h2>
-                <form action="#">
-                    <div class="input-box">
-                 <label>Email</label>
-                    <div class="input-container">
-                        <input type="email" required>
-                        <img src="mail (1).png" alt="Email">
-                    </div>
+   <div class="wrapper">
+    <div class="form-box">
+        <h2>Player Login...</h2>
+        
+        <form>
+            <div class="input-group">
+                <label>Email</label>
+                <div class="input-field-wrapper">
+                    <input type="email" required>
+                    <img src="mail (1).png" alt="mail">
                 </div>
-
-                <div class="input-box">
-                    <label>Password</label>
-                    <div class="input-container">
-                        <input type="password" required>
-                        <img src="lock.png" alt="Password">
-                    </div>
-                </div>
-                <button type="submit" class="btn-submit">Login</button>
-                    <div class="remember-forgot">
-                        <label> <input type="checkbox">Remember me</label>
-                        <a href="#">Forgot Password?</a>
-                        <br>
-
-                      
-                    </div>
-
-
-                    <div class="login-register">
-                        <p>Don't have an account? <a href="#" class="register-link">Register</a></p>
-                    </div>
-
-
-                </form>
             </div>
-        </div>
+
+            <div class="input-group">
+                <label>Password</label>
+                <div class="input-field-wrapper">
+                    <input type="password" required>
+                    <img src="lock.png" alt="lock">
+                </div>
+            </div>
+
+            <div class="remember-forgot">
+                <label><input type="checkbox"> Remember me</label>
+                <a href="#">Forgot?</a>
+            </div>
+
+            <button type="submit" class="btn-submit">Login</button>
+
+            <div class="login-register">
+                <p>No account? <a href="#">Register</a></p>
+            </div>
+        </form>
     </div>
+</div>
     
 
 </body>
